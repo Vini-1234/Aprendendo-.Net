bff627e30d960d2e4a30d772857490cfb7489f9b
Vinicius Suriano <vinicius_suriano@hotmail.com>
2020-06-11 16:03:54 -0300

    seller e serviço de exceção customizado atualizado

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 
@@ -9,9 +6,9 @@ namespace SalesWebMvc.Model
 {
     public class SalesWebMvcContext : DbContext
     {
-        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
-            : base(options)
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : base(options)
         {
+
         }
 
         public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
